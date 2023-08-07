06994ba1caa64d9a799df7147b4e9e939815f640
Vinay Kumar Yadav <vinay.kumar.yadav@intel.com>
2023-08-07 18:34:03 +0530

    P4_Plugin: improve build time
      -Add make -j<num of cpu> to improve build time.
    
    Signed-off-by: Vinay Kumar Yadav <vinay.kumar.yadav@intel.com>

--- env_setup_acc.sh
+++ env_setup_acc.sh
@@ -11,9 +11,32 @@ export C_INCLUDE_PATH=$DEPS_INSTALL/include
 export CPLUS_INCLUDE_PATH=$DEPS_INSTALL/include
 export LIBRARY_PATH=$DEPS_INSTALL/lib
 export PATH=$DEPS_INSTALL/bin:$PATH
-export NUM_THREADS="-j24"
 
 export LD_LIBRARY_PATH=$DEPS_INSTALL/lib:$DEPS_INSTALL/lib64:$LD_LIBRARY_PATH
 export PKG_CONFIG_PATH=$DEPS_INSTALL/lib/pkgconfig:$DEPS_INSTALL/lib64/pkgconfig
 export C_INCLUDE_PATH=$DEPS_INSTALL/include
 export CPLUS_INCLUDE_PATH=$DEPS_INSTALL/include
+
+# First check if NUM_CORES and NUM_THREADS are already set, and exit if so.
+   if [[ -n $NUM_CORES && -n $NUM_THREADS ]]
+   then
+      echo "NUM_CORES and NUM_THREADS already set."
+      return
+   fi
+
+   nproc_exist=$(command -v nproc)
+   if [ -n "$nproc_exist" ];
+   then
+       NUM_CORES=$(nproc --all)
+       echo "Num cores on a system: $NUM_CORES"
+       if [ "$NUM_CORES" -gt 4 ]
+       then
+           NUM_THREADS=$((NUM_CORES / 4))
+           NUM_THREADS=-j$NUM_THREADS
+       else
+           NUM_THREADS=-j${NUM_CORES}
+       fi
+    else
+        NUM_CORES=1
+        NUM_THREADS=-j1
+    fi
